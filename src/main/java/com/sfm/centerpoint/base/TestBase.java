d7c8782182d1e16ab3ff010fd1b3d475fb117b63
SreekanthK <sreekanthkatar@gmail.com>
2018-09-05 10:15:30 +0530

    Folder Path Changed

--- src/main/java/com/sfm/centerpoint/base/TestBase.java
+++ src/main/java/com/sfm/centerpoint/base/TestBase.java
@@ -26,7 +26,7 @@ public class TestBase {
 	{
 		try {
 			prop = new Properties();
-			FileInputStream fi = new FileInputStream("D:\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\config\\config.properties");
+			FileInputStream fi = new FileInputStream("D:\\Repositories\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\config\\config.properties");
 			prop.load(fi);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -43,26 +43,26 @@ public class TestBase {
 		if(browserName.equals("chrome"))
 		{
 			Reporter.log("Launching Chrome Browser..",true);
-			System.setProperty("webdriver.chrome.driver", "D:\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "D:\\Repositories\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\chromedriver.exe");
 			driver = new ChromeDriver();
 			Reporter.log("Chrome Browser Launched",true);
 		}
 		else if(browserName.equals("firefox"))
 		{
 			Reporter.log("Launching Firefox Browser..",true);
-			System.setProperty("webdriver.gecko.driver", "D:\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "D:\\Repositories\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			Reporter.log("Firefox Browser Launched",true);
 		}
 		else if (browserName.equals("opera")) {
 			Reporter.log("Launching Opera Browser..",true);
-			System.setProperty("webdriver.opera.driver", "D:\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\operadriver.exe");
+			System.setProperty("webdriver.opera.driver", "D:\\Repositories\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\operadriver.exe");
 			driver = new OperaDriver();
 			Reporter.log("Opera Browser Launched",true);
 		}
 		else if (browserName.equals("ie")) {
 			Reporter.log("Launching Internet Explorer Browser..",true);
-			System.setProperty("webdriver.ie.driver", "D:\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver", "D:\\Repositories\\SFM_POM\\src\\main\\java\\com\\sfm\\centerpoint\\browsers\\IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
 			Reporter.log("IE Browser Launched",true);
 		}
