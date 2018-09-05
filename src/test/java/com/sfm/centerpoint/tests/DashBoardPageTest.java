b02b19510811c12c6b22069eb130e4b2f8a17095
SreekanthK <sreekanthkatar@gmail.com>
2018-09-05 12:11:55 +0530

    Making Pass

--- src/test/java/com/sfm/centerpoint/tests/DashBoardPageTest.java
+++ src/test/java/com/sfm/centerpoint/tests/DashBoardPageTest.java
@@ -33,7 +33,7 @@ public class DashBoardPageTest extends TestBase{
 	public void VerifyDashboardPage()
 	{
 		boolean flag = dashboardpage.SFMLogo();
-		Assert.assertFalse("SFMLogo is not displayed", flag);
+		Assert.assertTrue("SFMLogo is not displayed", flag);
 	}
 
 	@AfterMethod
